d9d9e8eb7954e0cbd5660299f107e77b98503c35
MOM — Maks <mom-maks@MacBook-Air-MOM-2.local>
2021-12-04 04:25:24 +0500

    added

--- backend/settings.py
+++ backend/settings.py
@@ -54,10 +54,10 @@ MIDDLEWARE = [
 
 REST_FRAMEWORK = {
 
-    'DEFAULT_RENDERER_CLASSES': (
-        'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
-        'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
-    ),
+    # 'DEFAULT_RENDERER_CLASSES': (
+    #     'djangorestframework_camel_case.render.CamelCaseJSONRenderer',
+    #     'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
+    # ),
 
     # 'DEFAULT_PARSER_CLASSES': (
     #     'djangorestframework_camel_case.parser.CamelCaseFormParser',
