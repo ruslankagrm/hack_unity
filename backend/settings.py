6030ebd9655845f56e8dea12cda8330a4f7e1493
MOM — Maks <mom-maks@MacBook-Air-MOM-2.local>
2021-12-04 04:23:21 +0500

    added

--- backend/settings.py
+++ backend/settings.py
@@ -59,11 +59,11 @@ REST_FRAMEWORK = {
         'djangorestframework_camel_case.render.CamelCaseBrowsableAPIRenderer',
     ),
 
-    'DEFAULT_PARSER_CLASSES': (
-        'djangorestframework_camel_case.parser.CamelCaseFormParser',
-        'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
-        'djangorestframework_camel_case.parser.CamelCaseJSONParser',
-    ),
+    # 'DEFAULT_PARSER_CLASSES': (
+    #     'djangorestframework_camel_case.parser.CamelCaseFormParser',
+    #     'djangorestframework_camel_case.parser.CamelCaseMultiPartParser',
+    #     'djangorestframework_camel_case.parser.CamelCaseJSONParser',
+    # ),
 }
 
 ROOT_URLCONF = 'backend.urls'
